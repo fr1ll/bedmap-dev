365fb17aa1bff87e4051cde82150af1557ebce16
Will Sanger <willsa@hey.com>
2025-01-26 13:25:11 -0600

    Initial commit

---
+++ tests/test_dummy.py
@@ -0,0 +1,7 @@
+from jlp.python_template.hello import say_hi
+
+
+def test_dummy() -> None:
+    """Dummy test for checking that CI works"""
+    greet = say_hi("world")
+    assert greet == "Hi, world"
